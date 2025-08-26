7c3309997f527c9bc0af022f416de04e0477a67f
jdcorderor <jdcorderor@gmail.com>
2025-08-26 13:05:10 -0400

    Fixed date formatter

--- hooks/homePageHooks.ts
+++ hooks/homePageHooks.ts
@@ -1,13 +1,13 @@
 // Format date to a readable string
 export function formatDate(time: string): string {
-  const isoTime = new Date(time).toISOString();
-  const date = new Date(isoTime);
-  return date.toLocaleString("es-VE", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
+  const date = new Date(time);
+  return new Intl.DateTimeFormat('es-VE', {
+    timeZone: 'America/Caracas',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
     hour12: false
-  });
+  }).format(date);
 }
